b950dc9ca11378f8bcb731a6f20d5c6252648384
Casey Asher <casey.e.asher@outlook.com>
2019-12-06 21:04:25 -0600

    stubbed angular project and added error pages

--- ngPersonalSite/src/app/app.module.ts
+++ ngPersonalSite/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ApphousingComponent } from './components/apphousing/apphousing.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ApphousingComponent,
   ],
   imports: [
     BrowserModule,
---
+++ ngPersonalSite/src/app/components/apphousing/apphousing.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-apphousing',
+  templateUrl: './apphousing.component.html',
+  styleUrls: ['./apphousing.component.css']
+})
+export class ApphousingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
